6554aca7086446810048fceff1f01319489e0aec
onthesub <aaron.schieffer@gmail.com>
2024-11-26 21:18:09 -0600

    Connect to backend

---
+++ src/InventoryList.js
@@ -0,0 +1,30 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+function InventoryList() {
+  const [inventory, setInventory] = useState([]);
+
+  useEffect(() => {
+    const fetchInventory = async () => {
+      const response = await axios.get('http://localhost:3000/inventory');
+      setInventory(response.data);
+    };
+
+    fetchInventory();
+  }, []);
+
+  return (
+    <div>
+      <h2>Inventory</h2>
+      <ul>
+        {inventory.map(item => (
+          <li key={item._id}>
+            {item.name} - {item.quantity} - {item.location}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default InventoryList;
